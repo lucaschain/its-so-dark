8a81e6c740a0ba83c39d144be52112cf66389bae
Lucas Chain <sou@lucaschain.com.br>
2019-06-10 23:53:39 -0300

    harry f**ing potter

--- src/synth/grid.js
+++ src/synth/grid.js
@@ -13,12 +13,36 @@ const createKeyForVertex = (
   correctPath: OrderedSet<Vertex>,
   vertex: Vertex
 ): Key => {
-  const firstNote = new Note(220)
+  const firstNote = new Note(130.81)
   const pos = correctPath.toArray().findIndex((subject) => (
     vertex.x === subject.x && vertex.y === subject.y
   ))
 
-  const scale = [ 0, 2, 4, 5, 7, 9, 11 ]
+  const noteToNum = (note: string): number => {
+    const octavesUp = (note.match(/\+/g) || []).length
+    const octavesDown = (note.match(/\-/g) || []).length
+
+    const pureNote = note.replace(/(\+|\-)/g, '')
+    return {
+      "Cb": -1,
+      "C": 0,
+      "C#": 1, "Db": 1,
+      "D": 2,
+      "D#": 3, "Eb": 3,
+      "E": 4,
+      "F": 5,
+      "F#": 6, "Gb": 6,
+      "G": 7,
+      "G#": 8, "Ab": 8,
+      "A": 9,
+      "A#": 10, "Bb": 10,
+      "B": 11
+    }[pureNote] + (octavesUp * 12) - (octavesDown * 12) + 12
+  }
+
+  const scale = "-B E G F# E B A F# E G F# Eb E -B -B E G F# E B +D +Db +C Ab +C B Bb Eb G E G B G B G +C B Bb F# G B Bb Eb E B G B G B G +D +Db +C Ab +C B Bb Eb G E"
+    .split(' ')
+    .map(noteToNum)
 
   if (pos > -1) {
     const positionInPath = correctPath.size - 1 - pos
