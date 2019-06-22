844d73e2f1beeb528098686bf6b5841fa6546992
Lucas Chain <sou@lucaschain.com.br>
2019-06-22 20:58:10 -0300

    Deleting unused code

--- src/common/vertex.js
+++ src/common/vertex.js
@@ -4,26 +4,6 @@ export type Vertex = {
   y: number
 }
 
-export const moveUp = (position: Vertex, amount: number = 1) => ({
-  ...position,
-  y: position.y - amount
-})
-
-export const moveDown = (position: Vertex, amount: number = 1) => ({
-  ...position,
-  y: position.y + amount
-})
-
-export const moveLeft = (position: Vertex, amount: number = 1) => ({
-  ...position,
-  x: position.x - amount
-})
-
-export const moveRight = (position: Vertex, amount: number = 1) => ({
-  ...position,
-  x: position.x + amount
-})
-
 export const distance = (pointA: Vertex, pointB: Vertex) => {
   const a = pointA.x - pointB.x
   const b = pointA.y - pointB.y
